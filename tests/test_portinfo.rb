525db631b84746643600fd06187d0b33e1f8a820
Matthew D. Fuller <fullermd@over-yonder.net>
2020-02-01 19:04:56 -0600

    Go ahead and accept this multi-line array alignement.

--- tests/test_portinfo.rb
+++ tests/test_portinfo.rb
@@ -16,35 +16,35 @@ class TestPortInfo < Test::Unit::TestCase
 
     portinfo = PortInfo.new(SAMPLE1)
     assert_equal(['ruby-perl', '0.2.7',
-                 'lang/ruby-perl', 'lang/ruby-perl/pkg-descr',
-                 '/usr/local', 'knu@FreeBSD.org',
-                 'A Ruby extension module to use the functions of Perl from Ruby',
-                 ['lang', 'ruby', 'perl5'],
-                 ['ruby-1.6.2.2001.02.05'], ['ruby-1.6.2.2001.02.05'],
-                 'http://www.yoshidam.net/Ruby.html#perl'],
+                  'lang/ruby-perl', 'lang/ruby-perl/pkg-descr',
+                  '/usr/local', 'knu@FreeBSD.org',
+                  'A Ruby extension module to use the functions of Perl from Ruby',
+                  ['lang', 'ruby', 'perl5'],
+                  ['ruby-1.6.2.2001.02.05'], ['ruby-1.6.2.2001.02.05'],
+                  'http://www.yoshidam.net/Ruby.html#perl'],
                  [portinfo.pkgname.name, portinfo.pkgname.version.to_s,
-                 portinfo.origin, portinfo.descr_file,
-                 portinfo.prefix, portinfo.maintainer,
-                 portinfo.comment,
-                 portinfo.categories,
-                 portinfo.build_depends, portinfo.run_depends,
-                 portinfo.www])
+                  portinfo.origin, portinfo.descr_file,
+                  portinfo.prefix, portinfo.maintainer,
+                  portinfo.comment,
+                  portinfo.categories,
+                  portinfo.build_depends, portinfo.run_depends,
+                  portinfo.www])
     
     portinfo = PortInfo.new(SAMPLE2)
     assert_equal(['ruby-byaccr', '0.0_1',
-                 'devel/ruby-byaccr', 'devel/ruby-byaccr/pkg-descr',
-                 '/usr/local', 'knu@FreeBSD.org',
-                 'Parser generator for ruby based on \'Berkeley Yacc\' and \'Berkeley Yacc for Java\'',
-                 ['devel', 'ruby'],
-                 [], [],
-                 nil],
+                  'devel/ruby-byaccr', 'devel/ruby-byaccr/pkg-descr',
+                  '/usr/local', 'knu@FreeBSD.org',
+                  'Parser generator for ruby based on \'Berkeley Yacc\' and \'Berkeley Yacc for Java\'',
+                  ['devel', 'ruby'],
+                  [], [],
+                  nil],
                  [portinfo.pkgname.name, portinfo.pkgname.version.to_s,
-                 portinfo.origin, portinfo.descr_file,
-                 portinfo.prefix, portinfo.maintainer,
-                 portinfo.comment,
-                 portinfo.categories,
-                 portinfo.build_depends, portinfo.run_depends,
-                 portinfo.www])
+                  portinfo.origin, portinfo.descr_file,
+                  portinfo.prefix, portinfo.maintainer,
+                  portinfo.comment,
+                  portinfo.categories,
+                  portinfo.build_depends, portinfo.run_depends,
+                  portinfo.www])
   end
   
   def test_to_s
